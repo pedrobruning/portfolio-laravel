4eb78f13fc4792cff3188b10d233d6f2d8830e5e
Pedro Henrique Bruning de Oliveira Schier <pedrohboschier@gmail.com>
2021-09-25 16:56:44 -0300

    feat: Removed language from resume relationship.

--- database/migrations/2021_09_25_183951_create_language_levels_table.php
+++ database/migrations/2021_09_25_183951_create_language_levels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateResumeLanguageLevelsTable extends Migration
+class CreateLanguageLevelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateResumeLanguageLevelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('resume_language_levels', function (Blueprint $table) {
+        Schema::create('language_levels', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
@@ -26,6 +26,6 @@ class CreateResumeLanguageLevelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('resume_language_levels');
+        Schema::dropIfExists('language_levels');
     }
 }
--- database/migrations/2021_09_25_184130_create_resume_languages_table.php
+++ database/migrations/2021_09_25_184130_create_resume_languages_table.php
@@ -17,7 +17,7 @@ class CreateResumeLanguagesTable extends Migration
             $table->id();
             $table->foreignId('resume_id')->constrained();
             $table->foreignId('user_id')->constrained();
-            $table->foreignId('resume_language_level_id')->constrained();
+            $table->foreignId('language_level_id')->constrained();
             $table->foreignId('language_id')->constrained();
             $table->timestamps();
         });
