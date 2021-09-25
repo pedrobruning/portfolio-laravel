f082d1f85402e8b9e6d21ec6ed07ff2500dbc950
Pedro Henrique Bruning de Oliveira Schier <pedrohboschier@gmail.com>
2021-09-25 18:16:21 -0300

    feat: Changed migration and migration class name.

--- database/migrations/2021_09_25_184421_create_portfolio_projects_table.php
+++ database/migrations/2021_09_25_184421_create_portfolio_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePortfolioItemsTable extends Migration
+class CreatePortfolioProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -32,6 +32,6 @@ class CreatePortfolioItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('portfolio_items');
+        Schema::dropIfExists('portfolio_projects');
     }
 }
